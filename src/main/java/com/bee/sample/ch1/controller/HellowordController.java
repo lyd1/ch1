a30fdcc7da6c8cc1235cecab087b556b97c0f167
linyd <601921796@qq.com>
2018-01-22 15:40:21 +0800

    初始提交

---
+++ src/main/java/com/bee/sample/ch1/controller/HellowordController.java
@@ -0,0 +1,13 @@
+package com.bee.sample.ch1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class HellowordController {
+    @RequestMapping("/")
+    @ResponseBody
+    public  String say(){
+        return "hello spring boot";
+    }
+}
